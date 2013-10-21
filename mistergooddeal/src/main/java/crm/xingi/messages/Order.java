d9aebd142d9efd8dd87ad32df63d55063774f51b
xinyi_guo <falcon2002gxy@gmail.com>
2013-10-21 16:24:53 +0200

    Add get et set pour les classes

--- mistergooddeal/src/main/java/crm/xingi/messages/Order.java
+++ mistergooddeal/src/main/java/crm/xingi/messages/Order.java
@@ -9,6 +9,35 @@ public class Order {
 	@XmlElement(required = true, name = "identifier")
 	public int identifier;
 	
+	@XmlElement(required = true, name = "statu")
+	public String statu;
+	
 	@XmlElement(required = true, name = "products")
 	public List<Product> products;
+
+	public int getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(int identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getStatu() {
+		return statu;
+	}
+
+	public void setStatu(String statu) {
+		this.statu = statu;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	
+	
 }

8328bec7195fc3f66e1303e13d7c8c4d31870b1c
mizrahi <mizrahi@mizrahi.EPU>
2013-10-28 13:58:40 +0100

    Ajout liste dans Delivery

--- mistergooddeal/src/main/java/warehouse/data/Delivery.java
+++ mistergooddeal/src/main/java/warehouse/data/Delivery.java
@@ -1,11 +1,12 @@
 package warehouse.data;
 
 import java.util.Date;
+import java.util.List;
 
 public class Delivery {
 	private String transporter;
 	private Date dateOfPickUp;
-	private int refProduct;
+	private List<Product> listProduct;
 	private int quantity;
 	
 	public String getTransporter() {
@@ -20,17 +21,17 @@ public class Delivery {
 	public void setDateOfPickUp(Date dateOfPickUp) {
 		this.dateOfPickUp = dateOfPickUp;
 	}
-	public int getRefProduct() {
-		return refProduct;
-	}
-	public void setRefProduct(int refProduct) {
-		this.refProduct = refProduct;
-	}
 	public int getQuantity() {
 		return quantity;
 	}
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
+	}
+	public List<Product> getListProduct() {
+		return listProduct;
+	}
+	public void setListProduct(List<Product> listProduct) {
+		this.listProduct = listProduct;
 	}
 
 }

017a531201cf727cfe779ab0e06177026a40c71c
giacogiac <giacogiac@github.com>
2013-11-24 22:43:12 +0100

    giacomo final

---
+++ mistergooddeal/src/main/java/internal/data/Customer.java
@@ -0,0 +1,11 @@
+package internal.data;
+
+public class Customer {
+	String username;
+	String email;
+	public Customer(String username, String email) {
+		super();
+		this.username = username;
+		this.email = email;
+	}
+}
